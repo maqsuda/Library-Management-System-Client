a89b857de9279e2215c64e5e6409c5bfcb5112da
Maqsuda Binte Mahboob <maqsudamahboob@gmail.com>
2025-01-11 14:36:40 +0600

    Project Create

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,19 @@
+import {
+    createBrowserRouter,
+  } from "react-router-dom";
+import Main from "../Layout/Main";
+
+  
+  export const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children:[
+        {
+            path:"/",
+            element:<h2></h2>
+
+        }
+      ]
+    },
+  ]);
